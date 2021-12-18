a1fc277506d02443ae5fdc09699af9e06adc4e08
Allef Siqueira <allefsiqueira_6@hotmail.com>
2021-12-18 00:41:15 -0300

    Aprendendo Async e Await

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const chalk = require('chalk');
+const fs = require('fs')
 
-console.log(chalk.blue('vamos começar!'));
+const file = "./arquivos/texto1.md"
 
-const paragrafo = 'Texto retornado por uma função';
-
-function texto(string) {
-  return string;
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'não há arquivo no caminho.'))
 }
 
-console.log(texto(paragrafo));
+async function pegaArquivo(caminhoArquivo){
+  
+  try{
+    const encoding = 'utf-8'
+    const text = await fs.promises.readFile(caminhoArquivo, encoding)
+
+  }catch(erro){
+    trataErro(erro)
+  }
+}
+
+// function pegaArquivo(caminhoDoArquivo) {
+  //   const encoding = 'utf-8';
+  //   fs.promises
+  //   .readFile(caminhoDoArquivo, encoding)
+  //   .then((data) => chalk.green(console.log(texto)))
+  //   .catch((erro) => trataErro(erro))
+  // }
+  
+  // function pegaArquivo(caminhoDoArquivo) {
+  //   const encoding = 'utf-8';
+  //   fs.readFile(caminhoDoArquivo, encoding, (erro, data) => {
+  //     if (erro) {
+  //       trataErro(erro);
+  //     }
+  //     console.log(chalk.green(data));
+  //   })
+  // }
+
+pegaArquivo(file)
